59a5549bb1da5a3c11de0800de65df929d044413
sadvasquez <sadvasquez@misena.edu.co>
2023-06-09 22:51:57 -0500

    desarrollo APIREST

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const postRoute = require ('./routes/post');
+const res = require('express/lib/response');
+const req = require('express/lib/request');
 
 app.use('/servicios',postRoute);
 
@@ -14,7 +16,17 @@ app.get('/',(req,res)=>{
 
     res.send('hola mundo');
 
+
 });
+
+app.post('/',(req,res) => {
+
+    res.send('dato recibido');
+})
+
+
+
+
 
 Mongoose.connect('mongodb+srv://sadvasquez:<S%a%m0%443%2>@cluster1.4zvxfkz.mongodb.net/?retryWrites=true&w=majority',
 {usenewUrlParser:true},() =>{
